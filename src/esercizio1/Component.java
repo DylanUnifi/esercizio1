7487aa6427ce62c2f8a969ca9ade044c2db6211e
Dylan Fouepe <berkampdylan@gmail.com>
2023-05-22 14:43:20 +0200

    update class diagram

--- src/esercizio1/Component.java
+++ src/esercizio1/Component.java
@@ -5,13 +5,18 @@ import java.util.ArrayList;
 public abstract class Component {
 	protected int level = 0;
 	protected static ArrayList<Integer> failLevel = new ArrayList<>();
-	public void addChild(Component c) throws Exception {
-		throw new Exception("Operazione non supportata !!!");
-	}
+	
 	public ArrayList<Integer> getFailLevel(){
 		return failLevel;
 	}
+	
+	public void addChild(Component c) throws Exception {
+		throw new Exception("Operazione non supportata !!!");
+	}
+	
 	public abstract Boolean test();
+	
 	public abstract int getLevel();
+	
 	public abstract void setLevel(int level);
 }
